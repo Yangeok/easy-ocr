6f3ff0d85f64bf4c9f30d4b39d16fa76698bea1e
Yangwook Jeong <wooky92@naver.com>
2020-10-26 14:42:21 +0900

    Fix code structure

--- app.py
+++ app.py
@@ -4,14 +4,21 @@ import os
 from itertools import groupby
 
 base_path = '/root/.EasyOCR'
+output_path = '/app/output/'
+txt_ext = '.txt'
 replacable = 'l 먼문사 '
 
 reader = easyocr.Reader(['ko', 'en'])
+
+def get_path (path, filename, ext):
+    return f'{path}{key}{ext}'
 
 files = list(sorted(filter(lambda x: x != 'model', os.listdir(base_path))))
 for key, group in groupby(files, lambda x: x.split('.')[0]):
     for item in group:
         print(item)
-        with open(f'/app/output/{key}.txt', 'a+') as output:
+
+        f_name = get_path(output_path, key, txt_ext)
+        with open(f_name, 'a+') as output:
             result = reader.readtext(f'{base_path}/{item}', detail=0)
             output.write(' '.join(result).replace(replacable, '') + '\n')
